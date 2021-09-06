82c3c744cffae316c91e790b89490221eca854bb
angelraver <angeldavidflorentin@gmail.com>
2021-09-05 23:02:08 -0300

    primer deploy

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset=utf-8>
+  <title>Fuego Azul Admin</title>
+  <meta name=viewport content="width=device-width,initial-scale=1">
+    rel=stylesheet>
+</head>
+
+<body>
+<h1>hola mundo</h1>
+</body>
+</html>
