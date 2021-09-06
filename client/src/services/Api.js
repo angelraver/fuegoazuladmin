a333767c92024c541fb77dadfcc34eae8f74009c
angelraver <angeldavidflorentin@gmail.com>
2021-09-06 14:53:07 -0300

    Variable de entorno vue agregada

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
 export default() => {
-  return axios.create({
-    baseURL: `http://localhost:8001/api.php`
-    // baseURL: `http://localhost:8001/index.php`
-    // baseURL: `https://fuegoazuladmin.000webhostapp.com/api.php/`
-  })
+  const api = process.env.API
+  return axios.create({ baseURL: api })
 }
